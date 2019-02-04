626b23a4092c0f921d220a598e0baa46e9c7e4dd
Islam <denizzz9998882@gmail.com>
2019-02-04 21:14:43 +0600

    ccdcsd

---
+++ HWDZ/Array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HWDZ
+{
+    public class Array
+    {
+        private int[] mas;
+        public int Size { get; set; }
+        public Array(int size)
+        {
+            Size = size;
+            mas = new int[Size];
+        }
+        public int this[int i]
+        {
+            get
+            {
+                return mas[i];
+            }
+            set
+            {
+                mas[i] = value * value;
+            }
+        }
+
+    }
+}
---
+++ HWDZ/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HWDZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array array = new Array(2);
+            array[0] = 6;
+            array[1] = 10;
+           
+
+            for (int i = 0; i < array.Size; i++)
+            {
+                Console.Write(array[i] + "\t");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
